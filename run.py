a7ddaab6811572d176f56c16b719036256c9e6f6
Markus Böck <markus.boeck7800@hotmail.com>
2024-09-11 09:32:10 +0200

    split work

--- run.py
+++ run.py
@@ -1,28 +1,57 @@
 
 import os
 import subprocess
+import sys
 
-with open("results.csv", "w") as f:
-    f.write("width,height,count,time,GC,bytes,RAM\n")
+if len(sys.argv) == 1:
+    mul = 1
+    off = 0
+elif len(sys.argv) == 3:
+    mul = int(sys.argv[1])
+    off = int(sys.argv[2])
+else:
+    print("usage: run.py mul off")
+    exit()
+print(f"mul={mul} off={off}")
 
+cnt = -1
+for width in range(1,12+1):
+    for height in range(1, 12+1):
+        if height + width > 12:
+            continue
+
+        cnt += 1
+        if cnt % mul != off:
+            continue
+
+        print(f"{cnt}. Count for width={width} x height={height} ... ", end="", flush=True)
+        # subprocess.run(["src/connect4.out", "27", str(width), str(height)], check=True, capture_output=True)
+        print("finished.")
 
 for width in range(1,12+1):
     for height in range(1, 12+1):
         if height + width != 13:
             continue
-        
-        print(f"Count for width={width} x height={height} ... ", end="", flush=True)
-        subprocess.run(["src/connect4.out", "29", str(width), str(height)], check=False)
+        cnt += 1
+        if cnt % mul != off:
+            continue
+
+        print(f"{cnt}. Count for width={width} x height={height} ... ", end="", flush=True)
+        # subprocess.run(["src/connect4.out", "29", str(width), str(height)], check=False)
         print("finished.")
 
-# for width in range(1,12+1):
-#     for height in range(1, 12+1):
-#         if height + width > 12:
-#             continue
-        
-#         print(f"Count for width={width} x height={height} ... ", end="", flush=True)
-#         subprocess.run(["src/connect4.out", "27", str(width), str(height)], check=True, capture_output=True)
-#         print("finished.")
+exit()
+for width in range(1,12+1):
+    for height in range(1, 12+1):
+        if height + width != 14:
+            continue
+        cnt += 1
+        if cnt % mul != off:
+            continue
+
+        print(f"{cnt}. Count for width={width} x height={height} ... ", end="", flush=True)
+        # subprocess.run(["src/connect4.out", "30", str(width), str(height)], check=False)
+        print("finished.")
 
 # results = pandas.read_csv("results.csv")
 # results = results[['width', 'height', 'count']]
