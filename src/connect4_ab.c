c66819b9490397822e65a6485ffcc7e144dd7c4d
Markus Böck <markus.boeck7800@hotmail.com>
2024-11-10 13:04:32 -0800

    wdl after n nodes does not help

--- src/connect4_ab.c
+++ src/connect4_ab.c
@@ -242,6 +242,55 @@ int8_t alphabeta_plain2(uint64_t player, uint64_t mask, int8_t alpha, int8_t bet
     return alpha;
 }
 
+int8_t alphabeta_plain3(uint64_t player, uint64_t mask, int8_t alpha, int8_t beta, uint8_t ply, uint8_t depth) {
+    n_plain_nodes++;
+    uint64_t pos = player ^ mask;
+
+    if (alightment(pos) || mask == BOARD_MASK) {
+        return -MATESCORE + ply; // terminal is always lost
+    }
+    if (depth == 0) {
+        return 0;
+    }
+    if (MATESCORE - ply <= alpha) {
+        return alpha;
+    }
+
+    if ((n_plain_nodes & ((1UL << 16) - 1)) == 0) {
+        uint8_t res = probe_board_mmap(player, mask);
+        if (res == 0) {
+            return 0; // draw
+        }
+        if (res == 1) {
+            if (beta <= 0) {
+                // winning move but we now that opponent has win
+                return beta;
+            }
+        }
+    }
+
+    uint8_t moves[7] = {3, 2, 4, 1, 5, 0, 6};
+
+    int8_t value = alpha;
+    uint64_t move_mask = get_pseudo_legal_moves(mask);
+    for (uint8_t col = 0; col < WIDTH; col++) {
+        uint64_t move = move_mask & column_mask(moves[col]);
+        if (move) {
+            value = -alphabeta_plain3(player ^ mask, mask | move, -beta, -alpha, ply+1, depth-1);
+        
+            if (value > alpha) {
+                alpha = value;
+            }
+            if (value >= beta) {
+                alpha = beta;
+                break;
+            }
+        }
+    }
+    
+    return alpha;
+}
+
 #define PLIES_IN_MEMORY 1
 
 uint64_t n_nodes = 0;
@@ -359,14 +408,14 @@ int8_t iterdeep(uint64_t player, uint64_t mask, bool verbose, uint8_t ply) {
         // free_mmap(WIDTH, HEIGHT, depth+ply);
         // read_in_memory(WIDTH, HEIGHT, depth+ply);
         // if (res == 1) {
-        //     ab = alphabeta(c4, 1, 2, 0, depth, res);
+        //     ab = alphabeta(player, mask, 1, 2, ply, depth, res);
         //     if (ab > 1) {
-        //         ab = alphabeta(c4, 1, MATESCORE, 0, depth, res);
+        //         ab = alphabeta(player, mask, 1, MATESCORE, ply, depth, res);
         //     }
         // } else {
-        //     ab = alphabeta(c4, -2, -1, 0, depth, res);
+        //     ab = alphabeta(player, mask, -2, -1, ply, depth, res);
         //     if (ab < -1) {
-        //         ab = alphabeta(c4, -MATESCORE, -1, 0, depth, res);
+        //         ab = alphabeta(player, mask, -MATESCORE, -1, ply, depth, res);
         //     }   
         // }
         if (res == 1) {
@@ -378,7 +427,8 @@ int8_t iterdeep(uint64_t player, uint64_t mask, bool verbose, uint8_t ply) {
         double t = get_elapsed_time(t0, t1);
         if (verbose) {
             printf("depth = %u, ab = %d, ", depth, ab);
-            printf("n_nodes = %"PRIu64" in %.3fs (%.3f knps), ", n_nodes, t, n_nodes / t / 1000);
+            uint64_t N = n_nodes + n_plain_nodes;
+            printf("n_nodes = %"PRIu64" in %.3fs (%.3f knps), ", N, t, N / t / 1000);
             printf("tt_hits = %.4f, n_tt_collisions = %"PRIu64", wdl_cache_hits = %.4f\n", (double) n_tt_hits / n_nodes, n_tt_collisions, (double) n_wdl_cache_hits / n_nodes);
         }
         if (abs(ab) > 1) {
@@ -396,3 +446,17 @@ int8_t iterdeep(uint64_t player, uint64_t mask, bool verbose, uint8_t ply) {
         depth++;
     }
 }
+
+// depth = 20, ab = 1, n_nodes = 5307978179 in 17.685s (300144.423 knps), tt_hits = 0.0879, n_tt_collisions = 3, wdl_cache_hits = 0.0958
+// depth = 21, ab = 1, n_nodes = 7351908580 in 25.596s (287223.624 knps), tt_hits = 0.0820, n_tt_collisions = 10, wdl_cache_hits = 0.0927
+// depth = 22, ab = 1, n_nodes = 9051155364 in 31.566s (286737.955 knps), tt_hits = 0.0942, n_tt_collisions = 37, wdl_cache_hits = 0.0997
+// depth = 23, ab = 1, n_nodes = 11083683833 in 41.147s (269370.608 knps), tt_hits = 0.0922, n_tt_collisions = 105, wdl_cache_hits = 0.0976
+// depth = 24, ab = 1, n_nodes = 12508372215 in 47.008s (266091.663 knps), tt_hits = 0.1032, n_tt_collisions = 243, wdl_cache_hits = 0.1033
+// depth = 25, ab = 1, n_nodes = 13943420346 in 59.223s (235439.918 knps), tt_hits = 0.1011, n_tt_collisions = 634, wdl_cache_hits = 0.1000
+// depth = 26, ab = 1, n_nodes = 14889367219 in 63.888s (233055.360 knps), tt_hits = 0.1116, n_tt_collisions = 1351, wdl_cache_hits = 0.1048
+// depth = 27, ab = 1, n_nodes = 15612249620 in 75.870s (205777.375 knps), tt_hits = 0.1109, n_tt_collisions = 2968, wdl_cache_hits = 0.1028
+// depth = 28, ab = 1, n_nodes = 16093290967 in 79.682s (201968.675 knps), tt_hits = 0.1222, n_tt_collisions = 5988, wdl_cache_hits = 0.1054
+// depth = 29, ab = 1, n_nodes = 16440010056 in 100.056s (164307.505 knps), tt_hits = 0.1234, n_tt_collisions = 14488, wdl_cache_hits = 0.0992
+// depth = 30, ab = 1, n_nodes = 16464158110 in 100.628s (163613.320 knps), tt_hits = 0.1235, n_tt_collisions = 14982, wdl_cache_hits = 0.0987
+// depth = 31, ab = 59, n_nodes = 17220586240 in 293.089s (58755.446 knps), tt_hits = 0.1541, n_tt_collisions = 2080765, wdl_cache_hits = 0.0282
+// Position is 1 (59)
