acead84987aadc078ba1c71f8ae146e50bdeb064
Markus Böck <markus.boeck7800@hotmail.com>
2024-11-18 09:55:11 +0100

    it is very important to work on similar positions and share tt

--- src/connect4/probe/generate_openingbook.c
+++ src/connect4/probe/generate_openingbook.c
@@ -21,35 +21,81 @@
 int main(int argc, char const *argv[]) {
     setbuf(stdout,NULL); // do not buffer stdout
 
-    if (argc < 2) {
-        perror("Wrong number of arguments supplied: generate_ob.out folder width height\n");
+    if (argc < 3) {
+        perror("Wrong number of arguments supplied: generate_ob.out folder n_workers\n");
         exit(EXIT_FAILURE);
     }
 
     const char *folder = argv[1];
     chdir(folder);
 
+    char * succ;
+    uint8_t n_workers = (uint32_t) strtoul(argv[2], &succ, 10);
+
     make_mmaps_read_in_memory(WIDTH, HEIGHT); // change to read binary
 
-    init_tt(&tt, 28);
+#if GLOBAL_TT
+    init_tt(&tt, 30);
     init_wdl_cache(&wdl_cache, 24);
+#endif
 
     struct timespec t0, t1;
     double t;
 
     clock_gettime(CLOCK_REALTIME, &t0);
 
-    // fill_opening_book_multithreaded(0, 0, 8, 4);
-    fill_opening_book(0, 0, 8);
+    uint64_t player = 0;
+    uint64_t mask = 0;
+    // play_column(&player, &mask, 0);
+
+    uint8_t depth = 8;
+    openingbook_t* obs = (openingbook_t*) malloc(n_workers * sizeof(openingbook_t));
+    for (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
+        init_openingbook(&obs[worker_id], 20);
+    }
+
+
+    openingbook_t nondraw_positions;
+    init_openingbook(&nondraw_positions, 20);
+    enumerate_nondraw(&nondraw_positions, player, mask, depth);
+    printf("Non-draw positions: %"PRIu64"\n", nondraw_positions.count);
+
+    // uint64_t work_per_worker = nondraw_positions.count / n_workers + (nondraw_positions.count % n_workers != 0);
+    // uint64_t i = 0;
+    // for (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
+    //     for (uint64_t j = 0; j < work_per_worker; j++) {
+    //         if (i >= nondraw_positions.count) {
+    //             break;
+    //         }
+    //         add_position_value(&obs[worker_id], nondraw_positions.entries[i].key, nondraw_positions.entries[i].value);
+    //         i++;
+    //     }
+    //     printf("Worker %u: %"PRIu64" positions\n", worker_id, obs[worker_id].count);
+    // }
+
+    for (uint64_t i = 0; i < nondraw_positions.count; i++) {
+        uint64_t worker_id = i % n_workers;
+        // uint8_t worker_id = hash_64(nondraw_positions.entries[i].key) % n_workers;
+        add_position_value(&obs[worker_id], nondraw_positions.entries[i].key, nondraw_positions.entries[i].value);
+    }
+    for (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
+        printf("Worker %u: %"PRIu64" positions\n", worker_id, obs[worker_id].count);
+    }
+
+    fill_opening_book_multithreaded(obs, player, mask, depth, n_workers);
+
+    // fill_opening_book_multithreaded_2(obs, player, mask, depth, n_workers);
 
     clock_gettime(CLOCK_REALTIME, &t1);
     t = get_elapsed_time(t0, t1);
     
     printf("generated %u finished in %.3fs\n", cnt, t);
 
-    printf("tt: hits=%"PRIu64" collisions=%"PRIu64" (%.4f) \n", tt.hits, tt.collisions, (double) tt.collisions / tt.stored);
+#if GLOBAL_TT
+    // printf("tt: hits=%"PRIu64" collisions=%"PRIu64" (%.4f) \n", tt.hits, tt.collisions, (double) tt.collisions / tt.stored);
     free(tt.entries);
     free(wdl_cache.entries);
+#endif
 
     return 0;
 }
@@ -92,3 +138,16 @@ int main(int argc, char const *argv[]) {
 // worker 0: generated 25060 positions
 // tt: hits=142780 collisions=5009
 // generated 25060 finished in 8.592s
+
+
+// w6 x h5
+// 2^30 tt
+// depth 6
+//   single: 230s
+//   10 threads: 60s
+// depth 8
+//   single: 560s
+//   10 threads: 140s
+// depth 10:
+//   single: 960s
+//   10 threads: 240s
--- src/connect4/probe/openingbook.c
+++ src/connect4/probe/openingbook.c
@@ -70,6 +70,24 @@ uint8_t get_value_for_position(openingbook_t* ob, uint64_t key) {
     exit(EXIT_FAILURE);
 }
 
+uint8_t update_value_for_position(openingbook_t* ob, uint64_t key, uint8_t value) {
+    uint32_t target_bucket_ix = hash_64(key) & ob->mask;
+    uint32_t b = ob->buckets[target_bucket_ix];
+    openingbook_entry_t entry;
+
+    // try to find existing node
+    while (b != (uint32_t) -1) {
+        entry = ob->entries[b];
+        if (key == entry.key) {
+            entry.value = value;
+            return entry.value;
+        }
+        b = entry.next;
+    }
+    perror("No value for key.\n");
+    exit(EXIT_FAILURE);
+}
+
 void init_openingbook(openingbook_t* ob, uint64_t log2size) {
     assert(log2size <= 32);
     uint64_t size = ((uint64_t) 1) << log2size;
@@ -93,9 +111,48 @@ void init_openingbook(openingbook_t* ob, uint64_t log2size) {
 #include <pthread.h>
 #include <stdatomic.h>
 
+uint64_t mask_from_key(uint64_t key) {
+    uint64_t mask = 0;
+    while (true) {
+        key = (key & ~BOTTOM_MASK) >> 1;
+        if (key == 0) {
+            break;
+        }
+        mask |= key;
+    }
+    return mask;
+}
+
+void enumerate_nondraw(openingbook_t* ob, uint64_t player, uint64_t mask, uint8_t depth) {
+    if (depth == 0) {
+        uint64_t key = position_key(player, mask);
+        uint64_t _mask = mask_from_key(key);
+        uint64_t _player = key & _mask;
+        assert(_mask == mask);
+        assert(_player == player);
+        if (!has_position(ob, key)) {
+            int8_t res = probe_board_mmap(player, mask);
+            if (res != 0) {
+                add_position_value(ob, key, res);
+            }
+        }
+        return;
+    }
+    if (is_terminal(player, mask)) {
+        return;
+    }
+    for (uint8_t move = 0; move < WIDTH; move++) {
+        if (is_legal_move(player, mask, move)) {
+            play_column(&player, &mask, move);
+            enumerate_nondraw(ob, player, mask, depth-1);
+            undo_play_column(&player, &mask, move);
+        }
+    }
+}
+
 atomic_uint cnt = 0;
 
-void _fill_opening_book_worker(tt_t* tt, wdl_cache_t* wdl_cache, openingbook_t* ob, uint64_t player, uint64_t mask, uint8_t depth, uint8_t worker_id, uint8_t n_workers) {
+void _fill_opening_book_worker_2(tt_t* tt, wdl_cache_t* wdl_cache, openingbook_t* ob, uint64_t player, uint64_t mask, uint8_t depth, uint8_t worker_id, uint8_t n_workers) {
     if (depth == 0) {
         uint64_t key = position_key(player, mask);
         uint64_t hash = hash_64(key);
@@ -116,27 +173,14 @@ void _fill_opening_book_worker(tt_t* tt, wdl_cache_t* wdl_cache, openingbook_t* 
     for (uint8_t move = 0; move < WIDTH; move++) {
         if (is_legal_move(player, mask, move)) {
             play_column(&player, &mask, move);
-            _fill_opening_book_worker(tt, wdl_cache, ob, player, mask, depth-1, worker_id, n_workers);
+            _fill_opening_book_worker_2(tt, wdl_cache, ob, player, mask, depth-1, worker_id, n_workers);
             undo_play_column(&player, &mask, move);
         }
     }
 }
 
-tt_t tt;
-wdl_cache_t wdl_cache;
-
-void fill_opening_book_worker(uint64_t player, uint64_t mask, uint8_t depth, uint8_t worker_id, uint8_t n_workers) {
-    openingbook_t ob;
-    init_openingbook(&ob, 20);
-    _fill_opening_book_worker(&tt, &wdl_cache, &ob, player, mask, depth, worker_id, n_workers);
-    printf("worker %u: generated %"PRIu64" positions\n", worker_id, ob.count);
-}
-
-void fill_opening_book(uint64_t player, uint64_t mask, uint8_t depth) {
-    fill_opening_book_worker(player, mask, depth, 0, 1);
-}
-
 typedef struct OpeningBookPayload {
+    openingbook_t* ob;
     uint64_t player;
     uint64_t mask;
     uint8_t depth;
@@ -144,22 +188,93 @@ typedef struct OpeningBookPayload {
     uint8_t n_workers;
 } openingbook_payload_t;
 
+void _fill_opening_book_worker(tt_t* tt, wdl_cache_t* wdl_cache, openingbook_t* ob, uint64_t player, uint64_t mask, uint8_t depth, uint8_t worker_id, uint8_t n_workers) {
+    for (uint64_t i = 0; i < ob->count; i++) {
+        openingbook_entry_t* entry = &ob->entries[i];
+        uint64_t mask = mask_from_key(entry->key);
+        uint64_t player = entry->key & mask;
+        assert(position_key(player, mask) == entry->key);
+        uint8_t value = iterdeep(tt, wdl_cache, player, mask, 0, 0);
+        entry->value = value;
+        cnt++;
+        if (worker_id == 0) {
+            printf("... %u\r", cnt);
+        }
+    }
+}
 
+#define GLOBAL_TT 1
+
+#if GLOBAL_TT
+tt_t tt;
+wdl_cache_t wdl_cache;
+#endif
 
 void *fill_opening_book_multithreaded_worker(void* arg) {
     openingbook_payload_t* payload = (openingbook_payload_t*) arg;
     printf("Started worker %u\n", payload->worker_id);
-    fill_opening_book_worker(payload->player, payload->mask, payload->depth, payload->worker_id, payload->n_workers);
+
+#if !GLOBAL_TT
+    tt_t tt;
+    wdl_cache_t wdl_cache;
+    init_tt(&tt, 28);
+    init_wdl_cache(&wdl_cache, 24);
+#endif
+
+    _fill_opening_book_worker(&tt, &wdl_cache, payload->ob, payload->player, payload->mask, payload->depth, payload->worker_id, payload->n_workers);
+
+#if !GLOBAL_TT
+    free(tt.entries);
+    free(wdl_cache.entries);
+#endif
+
     return NULL;
 }
 
-void fill_opening_book_multithreaded(uint64_t player, uint64_t mask, uint8_t depth, uint8_t n_workers) {
+void fill_opening_book_multithreaded(openingbook_t* obs, uint64_t player, uint64_t mask, uint8_t depth, uint8_t n_workers) {
     pthread_t* tid = malloc(n_workers * sizeof(pthread_t));
     openingbook_payload_t* payloads = malloc(n_workers * sizeof(openingbook_payload_t));
 
     for (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
-        payloads[worker_id] = (openingbook_payload_t) {player, mask, depth, worker_id, n_workers};
+        payloads[worker_id] = (openingbook_payload_t) {&obs[worker_id], player, mask, depth, worker_id, n_workers};
         pthread_create(&tid[worker_id], NULL, fill_opening_book_multithreaded_worker, &payloads[worker_id]);
+    }
+    for  (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
+        pthread_join(tid[worker_id], NULL);
+    }
+
+    free(tid);
+    free(payloads);
+}
+
+void *fill_opening_book_multithreaded_worker_2(void* arg) {
+    openingbook_payload_t* payload = (openingbook_payload_t*) arg;
+    printf("Started worker %u\n", payload->worker_id);
+
+#if !GLOBAL_TT
+    tt_t tt;
+    wdl_cache_t wdl_cache;
+    init_tt(&tt, 28);
+    init_wdl_cache(&wdl_cache, 24);
+#endif
+
+    _fill_opening_book_worker_2(&tt, &wdl_cache, payload->ob, payload->player, payload->mask, payload->depth, payload->worker_id, payload->n_workers);
+
+#if !GLOBAL_TT
+    free(tt.entries);
+    free(wdl_cache.entries);
+#endif
+
+    return NULL;
+}
+
+void fill_opening_book_multithreaded_2(openingbook_t* obs, uint64_t player, uint64_t mask, uint8_t depth, uint8_t n_workers) {
+    pthread_t* tid = malloc(n_workers * sizeof(pthread_t));
+    openingbook_payload_t* payloads = malloc(n_workers * sizeof(openingbook_payload_t));
+
+    for (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
+        payloads[worker_id] = (openingbook_payload_t) {&obs[worker_id], player, mask, depth, worker_id, n_workers};
+        pthread_create(&tid[worker_id], NULL, fill_opening_book_multithreaded_worker_2, &payloads[worker_id]);
     }
     for  (uint8_t worker_id = 0; worker_id < n_workers; worker_id++) {
         pthread_join(tid[worker_id], NULL);
